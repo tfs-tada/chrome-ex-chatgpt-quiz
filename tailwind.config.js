c724c3462cda4144b31da4b519f4620ee8b3bb93
tfs-tada <tfs.tada@gmail.com>
2024-08-12 23:47:25 +0900

    add: tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  corePlugins: {
+    preflight: false,
+  },
+  theme: {
+    extend: {
+      colors: {
+        "zenn-primary": "#3ea8ff",
+        "qiita-primary": "#67cb1b",
+      },
+    },
+  },
+  plugins: [],
+};
